1101ff0af4ba74ad607dd034c4d35f719168c1bb
ntrolls <ntrolls@gmail.com>
2024-03-26 21:58:27 +0900

    initialize skeletons

--- instrumentation/foo.py
+++ instrumentation/foo.py
@@ -13,24 +13,6 @@ def norm(x):
 def fitness():
     return appr + norm(bdist)
 
-def branch_lte(_id, x, y):
-    global bdist, appr
-    appr = 2 - _id
-    bdist = x - y + K
-    return x <= y
-
-def branch_gte(_id, x, y):
-    global bdist, appr
-    appr = 2 - _id
-    bdist = y - x + K
-    return x >= y
-
-def branch_eq(_id, x, y):
-    global bdist, appr
-    appr = 2 - _id
-    bdist = abs(x - y)
-    return x == y
-
 # rewrite function foo so that whenever we call it, we get
 # the fitness for the target branch
 #
@@ -38,9 +20,9 @@ def branch_eq(_id, x, y):
 # approx and bdist directly
 
 def foo(x, y, z):
-    if branch_gte(0, z, 4):
-        if branch_lte(1, z, 10):
-            if branch_eq(2, x, y):
+    if z >= 4:
+        if z <= 10:
+            if x == y:
                 # target
                 return True
             else:
@@ -63,13 +45,7 @@ def init_random(len, max):
 def get_neighbour(sol):
     neighbours = []
     # generate neighbours of sol
-    for i in range(len(sol)):
-        n1 = sol[:]
-        n1[i] += 1
-        n2 = sol[:]
-        n2[i] -= 1
-        neighbours.append(n1)
-        neighbours.append(n2)
+
     return neighbours
 
 def evaluate(sol):
@@ -78,21 +54,6 @@ def evaluate(sol):
 
 # to return the best solution found within budget, and spent cost
 def hillclimbing(budget):
-    climb = True
-    spent = 0
-    sol = init_random(LEN, MAX)
-    s_fit = evaluate(sol)
-    spent += 1
-    while climb and spent < budget:
-        neighbours = get_neighbour(sol)
-        climb = False
-        for n in neighbours:
-            n_fit = evaluate(n)
-            spent += 1
-            if n_fit < s_fit:
-                sol = n[:]
-                s_fit = n_fit
-                climb = True
     return sol, spent
 
 sol, cost = hillclimbing(2000)
--- search/localsearch.py
+++ search/localsearch.py
@@ -17,122 +17,20 @@ def evaluate(solution):
 
 def init_random(len, max):
     # randomly initialise a solution of length len, of randrange(max)
-    sol = []
-    for i in range(len):
-        sol.append(random.randrange(max))
-    return sol
+    return None
 
 def get_neighbour(sol):
     neighbours = []
     # generate neighbours of sol
-    for i in range(len(sol)):
-        n1 = sol[:]
-        n1[i] += 1
-        n2 = sol[:]
-        n2[i] -= 1
-        neighbours.append(n1)
-        neighbours.append(n2)
     return neighbours
 
 # to return the best solution found within budget, and spent cost
 def hillclimbing(budget):
-    climb = True
-    spent = 0
-    sol = init_random(LEN, MAX)
-    s_fit = evaluate(sol)
-    spent += 1
-    while climb and spent < budget:
-        neighbours = get_neighbour(sol)
-        climb = False
-        for n in neighbours:
-            n_fit = evaluate(n)
-            spent += 1
-            if n_fit < s_fit:
-                sol = n[:]
-                s_fit = n_fit
-                climb = True
-    return sol, spent
-
-EXP = 0
-PAT = 1
-
-INC = 0
-DEC = 1
+    return None, None
 
 # to return the best solution found within budget, and spent cost
 def avm(budget):
-    
-    mode = EXP # current search mode: EXP or PAT
-    dirr = None # current move direction: INC or DEC
-    cv = 0 # index of current variable we are looking at
-    
-    step = 1 # we move 2**step in dirr direction in PAT mode
-    spent = 0 # record the number of fitness evaluation
-
-    explore_failed = [0] * LEN # record 1 if EXP move for variable failed
-
-    sol = init_random(LEN, MAX)
-    fit = evaluate(sol)
-    spent += 1
-    
-    while spent < budget:
-        # exploratory move
-        if mode == EXP:
-            # try increase and decrease
-            _inc = sol[:]
-            _inc[cv] += 1
-            _dec = sol[:]
-            _dec[cv] -= 1
-
-            # evaluate both increase and decrease
-            fit_inc = evaluate(_inc)
-            spent += 1
-            fit_dec = evaluate(_dec)
-            spent += 1
-
-            if fit_inc < fit: # increase is the right direction
-                dirr = INC 
-                sol = _inc[:]
-                fit = fit_inc
-                explore_failed[cv] = 0 # record that exploration succeeded
-                mode = PAT # switch to PATTERN move
-            elif fit_dec < fit: # decrease is the right direction
-                dirr = DEC
-                sol = _dec[:]
-                fit = fit_dec
-                explore_failed[cv] = 0
-                mode = PAT
-            else:
-                # check if we have failed with exploration of all variables
-                explore_failed[cv] = 1
-                if sum(explore_failed) == LEN:
-                    break
-                cv = (cv + 1) % LEN
-        elif mode == PAT:
-            delta = 2 ** step # the distnace we move this time
-            next_sol = sol[:] # where we end up when we move: first copy
-            # add delta
-            next_sol[cv] = next_sol[cv] + delta if dirr == INC else next_sol[cv] - delta
-            fit_next = evaluate(next_sol)
-            spent += 1
-            
-            # if fitness improves, move sol to next_sol, and increase step
-            if fit_next <= fit:
-                sol = next_sol[:]
-                step += 1
-            # overshooting: reset the step, set mode to EXP
-            else:
-                step = 1
-                mode = EXP
-                cv = (cv + 1) % LEN # try the next variable
-    return sol, spent
+    return None, None
 
 guess, cost = hillclimbing(BUDGET)
 print(guess, cost)
-
-guess, cost = avm(BUDGET)
-print(guess, cost)
-
-print(hidden)
-
-# print(guess, evaluate(guess))
